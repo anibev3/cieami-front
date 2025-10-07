5f59c0727f6536eec1f8e22a6e227e765e32f985
anibev3 <anibev3@gmail.com>
2025-10-07 12:03:16 +0000

    wip

--- src/routes/_authenticated/assignments/statistics.tsx
+++ src/routes/_authenticated/assignments/statistics.tsx
@@ -1,21 +1,22 @@
-import { createFileRoute, redirect } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 import AssignmentStatisticsPage from '@/features/assignments/statistics'
-import { useACLStore, UserRole } from '@/stores/aclStore'
+// import { useACLStore, UserRole } from '@/stores/aclStore'
 
 export const Route = createFileRoute('/_authenticated/assignments/statistics')({
   beforeLoad: async () => {
-    const { hasAnyRole, isInitialized } = useACLStore.getState()
-    const allowedRoles = [
-      UserRole.SYSTEM_ADMIN,
-      UserRole.CEO,
-      UserRole.ACCOUNTANT_MANAGER,
-      UserRole.ADMIN,
-    ]
+    // const { hasAnyRole, isInitialized } = useACLStore.getState()
+    // const allowedRoles = [
+    //   UserRole.SYSTEM_ADMIN,
+    //   UserRole.CEO,
+    //   UserRole.ACCOUNTANT_MANAGER,
+    //   UserRole.ADMIN,
+    //   UserRole.EXPERT_MANAGER,
+    // ]
 
-    // Rediriger uniquement si l'ACL est initialisée et que l'utilisateur n'est pas autorisé
-    if (isInitialized && !hasAnyRole(allowedRoles)) {
-      throw redirect({ to: '/403' })
-    }
+    // // Rediriger uniquement si l'ACL est initialisée et que l'utilisateur n'est pas autorisé
+    // if (isInitialized && !hasAnyRole(allowedRoles)) {
+    //   throw redirect({ to: '/403' })
+    // }
   },
   component: AssignmentStatisticsPage,
 })
